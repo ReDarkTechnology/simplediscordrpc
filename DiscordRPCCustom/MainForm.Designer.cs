d53803a429c085a2bedc86c4c3cf729c85b2bb82
ReDark Technology <53262904+ReDarkTechnology@users.noreply.github.com>
2021-04-23 15:20:38 +0700

    My first ever public commit
    A first push into this project.

---
+++ DiscordRPCCustom/MainForm.Designer.cs
@@ -0,0 +1,330 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fauzi
+ * Date: 4/23/2021
+ * Time: 11:23 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DiscordRPCCustom
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox7;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label9 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label1.Location = new System.Drawing.Point(12, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(103, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Discord RPC";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label2
+			// 
+			this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label2.Location = new System.Drawing.Point(13, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 14);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Application ID";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox1.Location = new System.Drawing.Point(93, 37);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(276, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label3.Location = new System.Drawing.Point(13, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 14);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Details";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox2.Location = new System.Drawing.Point(93, 60);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(276, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox3.Location = new System.Drawing.Point(93, 108);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(276, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox4.Location = new System.Drawing.Point(93, 84);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(276, 20);
+			this.textBox4.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label4.Location = new System.Drawing.Point(13, 87);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(74, 14);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "State";
+			// 
+			// label5
+			// 
+			this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label5.Location = new System.Drawing.Point(13, 111);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(74, 14);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Asset Icon";
+			// 
+			// label6
+			// 
+			this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label6.Location = new System.Drawing.Point(13, 135);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(74, 14);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Icon Text";
+			// 
+			// textBox5
+			// 
+			this.textBox5.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox5.Location = new System.Drawing.Point(93, 132);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(276, 20);
+			this.textBox5.TabIndex = 8;
+			// 
+			// label7
+			// 
+			this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label7.Location = new System.Drawing.Point(13, 227);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(275, 14);
+			this.label7.TabIndex = 10;
+			this.label7.Text = "Discord RPC isn\'t running";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.Control;
+			this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button1.Location = new System.Drawing.Point(294, 223);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Connect";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(93, 152);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(23, 24);
+			this.checkBox1.TabIndex = 12;
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label8
+			// 
+			this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label8.Location = new System.Drawing.Point(13, 157);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(74, 14);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "Timestamps";
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Discord Rich Presence";
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1MouseDoubleClick);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.Control;
+			this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button2.Location = new System.Drawing.Point(267, 9);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(48, 23);
+			this.button2.TabIndex = 14;
+			this.button2.Text = "Hide";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.SystemColors.Control;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+			this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button3.Location = new System.Drawing.Point(321, 9);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(48, 23);
+			this.button3.TabIndex = 15;
+			this.button3.Text = "Close";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label9
+			// 
+			this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label9.Location = new System.Drawing.Point(13, 202);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(74, 14);
+			this.label9.TabIndex = 17;
+			this.label9.Text = "Icon Text";
+			// 
+			// textBox6
+			// 
+			this.textBox6.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox6.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox6.Location = new System.Drawing.Point(93, 199);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(276, 20);
+			this.textBox6.TabIndex = 16;
+			// 
+			// label10
+			// 
+			this.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label10.Location = new System.Drawing.Point(13, 179);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(74, 14);
+			this.label10.TabIndex = 19;
+			this.label10.Text = "Asset Icon";
+			// 
+			// textBox7
+			// 
+			this.textBox7.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.textBox7.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox7.Location = new System.Drawing.Point(93, 176);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(276, 20);
+			this.textBox7.TabIndex = 18;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ClientSize = new System.Drawing.Size(381, 253);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Discord Rich Presence";
+			this.MouseDown += this.MainForm_MouseDown;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
---
+++ DiscordRPCCustom/MainForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using DiscordRPC;
+
+namespace DiscordRPCCustom
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private static DiscordRpcClient client;
+		private static bool isClientInitialized;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			if(File.Exists("preferences.ini")){
+				string fileRead = File.ReadAllText("preferences.ini");
+				string[] splittedRead = fileRead.Split(new char[] {'|'});
+				textBox1.Text = splittedRead[0];
+				textBox2.Text = splittedRead[1];
+				textBox3.Text = splittedRead[2];
+				textBox4.Text = splittedRead[3];
+				textBox5.Text = splittedRead[4];
+				checkBox1.Checked = bool.Parse(splittedRead[5]);
+				textBox6.Text = splittedRead[6];
+				textBox7.Text = splittedRead[7];
+			}
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(!isClientInitialized){
+				client = new DiscordRpcClient(textBox1.Text);
+				Assets assets = new Assets();
+				assets.LargeImageKey = textBox3.Text;
+				assets.LargeImageText = textBox5.Text;
+				assets.SmallImageKey = textBox7.Text;
+				assets.SmallImageText = textBox6.Text;
+				RichPresence richPresence = new RichPresence();
+				richPresence.Details = textBox2.Text;
+				richPresence.State = textBox4.Text;
+				richPresence.Assets = assets;
+				if(checkBox1.Checked){
+					richPresence.Timestamps = new Timestamps()
+					{
+						Start = DateTime.UtcNow
+					};
+				}
+				client.SetPresence(richPresence);
+				client.Initialize();
+				isClientInitialized = true;
+				label7.Text = "Discord RPC is running";
+				button1.Text = "Update";
+			}else{
+				Assets assets = new Assets();
+				assets.LargeImageKey = textBox3.Text;
+				assets.LargeImageText = textBox5.Text;
+				RichPresence richPresence = new RichPresence();
+				richPresence.Details = textBox2.Text;
+				richPresence.State = textBox4.Text;
+				richPresence.Assets = assets;
+				if(checkBox1.Checked){
+					richPresence.Timestamps = new Timestamps()
+					{
+						Start = DateTime.UtcNow
+					};
+				}
+				client.SetPresence(richPresence);
+			}
+			string saveData = textBox1.Text + "|" + textBox2.Text + "|" + textBox3.Text + "|" + textBox4.Text + "|" + textBox5.Text + "|" + checkBox1.Checked.ToString() + "|" + textBox6.Text + "|" + textBox7.Text;
+			File.WriteAllText("preferences.ini", saveData);
+		}
+		void NotifyIcon1MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Show();
+			notifyIcon1.Visible = false;
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			Hide();
+			notifyIcon1.Visible = true;
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HT_CAPTION = 0x2;
+		
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		public static extern bool ReleaseCapture();
+		//this.MouseDown += this.MainForm_MouseDown;
+		private void MainForm_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{     
+		    if (e.Button == MouseButtons.Left)
+		    {
+		        ReleaseCapture();
+		        SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+		    }
+		}
+	}
+}
